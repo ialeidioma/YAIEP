777b5e71d9e872c41d1af35f13794aae87c08742
Alessandro <s220240@dtu.dk>
2022-07-10 19:43:28 +0200

    start of the main menu

--- Source.cpp
+++ Source.cpp
@@ -1,25 +1,114 @@
 #include <SFML/Graphics.hpp>
+#include "MainMenu.h"
+using namespace sf;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow MENU(VideoMode(960, 720), "Main Menu", Style::Default);
+    MainMenu mainMenu(MENU.getSize().x, MENU.getSize().y);
+    
 
-    while (window.isOpen())
+    while (MENU.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        Event event;
+        while (MENU.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            if (event.type == Event::Closed)
+                MENU.close();
+            
+            if (event.type == Event::KeyReleased) {
+                if (event.key.code == Keyboard::Up) {
+                    mainMenu.MoveUp();
+                    break;
+                }
+                if (event.key.code == Keyboard::Down) {
+                    mainMenu.MoveDown();
+                    break;
+                }
+                if (event.key.code == Keyboard::Return) {
+                    RenderWindow Play(VideoMode(960, 720), "YAIEP");
+                    RenderWindow OPTIONS(VideoMode(960, 720), "Options");
+                    RenderWindow ABOUT(VideoMode(960, 720), "About");
+
+                    int x = mainMenu.MainMenuPressed();
+                    if (x == 0) {
+                        while (Play.isOpen())
+                        {
+                            Event aevent;
+                            while (Play.pollEvent(aevent))
+                            {
+                                if (aevent.type == Event::Closed)
+                                    Play.close();
+
+                                if (aevent.type == Event::KeyReleased) {
+                                    if (aevent.key.code == Keyboard::Escape) {
+                                        Play.close();
+                                    }
+                                }
+                            }
+                            OPTIONS.close();
+                            ABOUT.close();
+                            Play.clear();
+                            Play.display();
+
+                        }
+                    }
+                    if (x == 1)
+                    {
+                        while (OPTIONS.isOpen())
+                        {
+                            Event aevent;
+                            while (OPTIONS.pollEvent(aevent))
+                            {
+                                if (aevent.type == Event::Closed)
+                                    OPTIONS.close();
+
+                                if (aevent.type == Event::KeyReleased) {
+                                    if (aevent.key.code == Keyboard::Escape) {
+                                        OPTIONS.close();
+                                    }
+                                }
+                            }
+                            Play.close();
+                            ABOUT.close();
+                            OPTIONS.clear();
+                            OPTIONS.display();
+
+                        }
+                    }
+                    if (x == 2)
+                    {
+                        while (ABOUT.isOpen())
+                        {
+                            Event aevent;
+                            while (ABOUT.pollEvent(aevent))
+                            {
+                                if (aevent.type == Event::Closed)
+                                    ABOUT.close();
+
+                                if (aevent.type == Event::KeyReleased) {
+                                    if (aevent.key.code == Keyboard::Escape) {
+                                        ABOUT.close();
+                                    }
+                                }
+                            }
+                            Play.close();
+                            OPTIONS.close();
+                            ABOUT.clear();
+                            ABOUT.display();
+
+                        }
+                    }
+                    if (x == 3)
+                        MENU.close();
+                    break;
+                }
+            }
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        MENU.clear();
+        mainMenu.draw(MENU);
+        MENU.display();
     }
 
-    return 0;
-    //Iale TROLOLOLOLOL
 }
